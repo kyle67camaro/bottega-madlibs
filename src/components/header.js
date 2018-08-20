2e73408e682ae949b0282b872022fe1519278b2b
Kyle Webb <kyle67camaro@gmail.com>
2018-08-20 12:07:37 -0600

    Attempt One Push

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React fom 'react';
+
+const Header = () => [
+    return (
+        <div className = 'home'>
+            <h1>Bottega Madlibs</h1>
+            <p>Fill out the fields below and click the generate button to see.</p>
+        </div>
+    )
+]
+
+export default Header;
